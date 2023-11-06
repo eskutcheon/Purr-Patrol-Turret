9143428dde3219957c65f300a8756e5654318e95
jkutch326 <jkutch326@gmail.com>
2023-11-06 13:08:24 -0600

    moved VideoUtils to motion_tracking.py and replaced dependency on deprecated imutils library

--- src/turret.py
+++ src/turret.py
@@ -9,13 +9,16 @@ import atexit
 import sys
 import termios
 import contextlib
-# TODO: replace all usage of this with matplotlib or something
-import imutils
-# TODO: replace this with the Jetson library and track down usage - Adafruit should still be the same since it's an Adafruit board
+from copy import deepcopy
+# TODO: replace this with the Jetson library and track down usage - Adafruit should still be the same since the motor driver is an Adafruit board
     # relevant methods only found in Turret class
 import RPi.GPIO as GPIO
+# This is apparently deprecated:
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+# using their recommendation here:
+#https://github.com/adafruit/Adafruit_CircuitPython_MotorKit
 from config import *
+from motion_tracking import VideoUtils
 
 @contextlib.contextmanager
 def raw_mode(file):
@@ -25,115 +28,13 @@ def raw_mode(file):
     :return:
     """
     old_attrs = termios.tcgetattr(file.fileno())
-    new_attrs = old_attrs[:]
-    new_attrs[3] = new_attrs[3] & ~(termios.ECHO | termios.ICANON)
+    new_attrs = deepcopy(old_attrs)
+    new_attrs[3] &= ~(termios.ECHO | termios.ICANON)
     try:
         termios.tcsetattr(file.fileno(), termios.TCSADRAIN, new_attrs)
         yield
     finally:
         termios.tcsetattr(file.fileno(), termios.TCSADRAIN, old_attrs)
-
-# TODO: move this to its own file - gonna need heavy modification for our libraries, use case, and general updates to Python3
-class VideoUtils(object):
-    """
-    Helper functions for video utilities.
-    """
-    @staticmethod
-    def live_video(camera_port=0):
-        """
-        Opens a window with live video.
-        :param camera:
-        :return:
-        """
-        video_capture = cv2.VideoCapture(camera_port)
-        while True:
-            # Capture frame-by-frame
-            ret, frame = video_capture.read()
-            # Display the resulting frame
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        # When everything is done, release the capture
-        video_capture.release()
-        cv2.destroyAllWindows()
-
-    @staticmethod
-    def find_motion(callback, camera_port=0, show_video=False):
-        camera = cv2.VideoCapture(camera_port)
-        time.sleep(0.25)
-        # initialize the first frame in the video stream
-        firstFrame = None
-        tempFrame = None
-        count = 0
-        # loop over the frames of the video
-        while True:
-            # grab the current frame and initialize the occupied/unoccupied
-            # text
-            (grabbed, frame) = camera.read()
-            # if the frame could not be grabbed, then we have reached the end
-            # of the video
-            if not grabbed:
-                break
-            # resize the frame, convert it to grayscale, and blur it
-            frame = imutils.resize(frame, width=500)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-            # if the first frame is None, initialize it
-            if firstFrame is None:
-                print("Waiting for video to adjust...")
-                if tempFrame is None:
-                    tempFrame = gray
-                    continue
-                else:
-                    delta = cv2.absdiff(tempFrame, gray)
-                    tempFrame = gray
-                    tst = cv2.threshold(delta, 5, 255, cv2.THRESH_BINARY)[1]
-                    tst = cv2.dilate(tst, None, iterations=2)
-                    if count > 30:
-                        print("Done.\n Waiting for motion.")
-                        if not cv2.countNonZero(tst) > 0:
-                            firstFrame = gray
-                        else:
-                            continue
-                    else:
-                        count += 1
-                        continue
-            # compute the absolute difference between the current frame and
-            # first frame
-            frameDelta = cv2.absdiff(firstFrame, gray)
-            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-            # dilate the thresholded image to fill in holes, then find contours
-            # on thresholded image
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            c = VideoUtils.get_best_contour(thresh.copy(), 5000)
-            if c is not None:
-                # compute the bounding box for the contour, draw it on the frame,
-                # and update the text
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                callback(c, frame)
-            # show the frame and record if the user presses a key
-            if show_video:
-                cv2.imshow("Security Feed", frame)
-                key = cv2.waitKey(1) & 0xFF
-                # if the `q` key is pressed, break from the lop
-                if key == ord("q"):
-                    break
-        # cleanup the camera and close any open windows
-        camera.release()
-        cv2.destroyAllWindows()
-
-    @staticmethod
-    def get_best_contour(imgmask, threshold):
-        im, contours, hierarchy = cv2.findContours(imgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        best_area = threshold
-        best_cnt = None
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > best_area:
-                best_area = area
-                best_cnt = cnt
-        return best_cnt
 
 
 class Turret(object):
@@ -164,10 +65,10 @@ class Turret(object):
         :return:
         """
         print("Please calibrate the tilt of the gun so that it is level. Commands: (w) moves up, " \
-              "(s) moves down. Press (enter) to finish.\n")
+            "(s) moves down. Press (enter) to finish.\n")
         self.__calibrate_y_axis()
         print("Please calibrate the yaw of the gun so that it aligns with the camera. Commands: (a) moves left, " \
-              "(d) moves right. Press (enter) to finish.\n")
+            "(d) moves right. Press (enter) to finish.\n")
         self.__calibrate_x_axis()
         print("Calibration finished.")
 
